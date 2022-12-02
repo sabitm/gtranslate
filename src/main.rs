216b64cf457392c4a6bc0ea5a2aedb5f50456b86
sabitm <sabitmaulanaa@gmail.com>
2022-12-02 07:22:28 +0700

    chore: format code

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,49 @@
 use curl::easy::Easy;
-use serde_json::{Value};
+use serde_json::Value;
 use std::env;
 
 fn main() {
     std::process::exit(match translator() {
         Ok(_) => 0,
-        Err(err) => err
+        Err(err) => err,
     });
 }
 
 fn translator() -> Result<(), i32> {
     let (src_lang, trg_lang, args) = match parse_args() {
         Ok((s, t, a)) => (s, t, a),
-        Err(err) => return Err(err)
+        Err(err) => return Err(err),
     };
 
-    const URL_FRAGMENT: (&str, &str, &str) = ("https://translate.googleapis.com/translate_a/single?client=gtx&sl=", "&tl=", "&hl=en-US&dt=t&dt=bd&dj=1&source=icon&tk=316277.316277&q=");
+    const URL_FRAGMENT: (&str, &str, &str) = (
+        "https://translate.googleapis.com/translate_a/single?client=gtx&sl=",
+        "&tl=",
+        "&hl=en-US&dt=t&dt=bd&dj=1&source=icon&tk=316277.316277&q=",
+    );
 
     let mut buf = Vec::new();
     let mut easy = Easy::new();
     let enc_args = easy.url_encode(args.as_bytes());
-    easy.url(&[URL_FRAGMENT.0,
-             src_lang.as_str(),
-             URL_FRAGMENT.1,
-             trg_lang.as_str(),
-             URL_FRAGMENT.2,
-             enc_args.as_str()].concat()).unwrap();
+    easy.url(
+        &[
+            URL_FRAGMENT.0,
+            src_lang.as_str(),
+            URL_FRAGMENT.1,
+            trg_lang.as_str(),
+            URL_FRAGMENT.2,
+            enc_args.as_str(),
+        ]
+        .concat(),
+    )
+    .unwrap();
     {
         let mut transfer = easy.transfer();
-        transfer.write_function(|data| {
+        transfer
+            .write_function(|data| {
                 buf.extend_from_slice(data);
                 Ok(data.len())
-            }).unwrap();
+            })
+            .unwrap();
         transfer.perform().unwrap();
     }
 
@@ -39,10 +51,13 @@ fn translator() -> Result<(), i32> {
         Ok(val) => val,
         Err(_) => {
             eprintln!("source or target language not found");
-            return Err(3)
+            return Err(3);
         }
     };
-    println!("translate: {}", res["sentences"][0]["trans"].as_str().unwrap());
+    println!(
+        "translate: {}",
+        res["sentences"][0]["trans"].as_str().unwrap()
+    );
 
     if res["dict"][0]["pos"] != serde_json::Value::Null {
         for i in res["dict"].as_array().unwrap().iter() {
@@ -60,7 +75,7 @@ fn translator() -> Result<(), i32> {
 fn parse_args() -> Result<(String, String, String), i32> {
     if env::args().len() <= 5 {
         usage();
-        return Err(1)
+        return Err(1);
     }
 
     let (mut src_lang, mut trg_lang) = (String::new(), String::new());
@@ -69,22 +84,24 @@ fn parse_args() -> Result<(String, String, String), i32> {
         ("-s", "-t") => {
             src_lang.push_str(args[1].as_str());
             trg_lang.push_str(args[3].as_str());
-        },
+        }
         ("-t", "-s") => {
             trg_lang.push_str(args[1].as_str());
             src_lang.push_str(args[3].as_str());
-        },
+        }
         (_, _) => {
             eprintln!("source and target language required");
-            return Err(2)
+            return Err(2);
         }
     };
 
-    let args: String = args.drain(4..)
+    let args: String = args
+        .drain(4..)
         .map(|mut x| {
             x.push(' ');
             x
-        }).collect();
+        })
+        .collect();
 
     Ok((src_lang, trg_lang, args))
 }
