dfae6887596cafbf98856896b892de02c7040754
sabitm <sabitmaulanaa@gmail.com>
2022-12-15 07:08:55 +0700

    fix: hardcode dns nameserver

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
+use std::io;
+use std::net::{IpAddr, SocketAddr};
+
 use anyhow::{bail, Result};
+use rsdns::clients::{std::Client, ClientConfig};
+use rsdns::{constants::Class, records::data::A};
 use ureq;
+
+fn to_ioerror<T>(err: T) -> io::Error
+where
+    T: std::error::Error + Send + Sync + 'static,
+{
+    io::Error::new(io::ErrorKind::Other, err)
+}
+
+fn resolve(qname: &str) -> io::Result<Vec<SocketAddr>> {
+    let qname: Vec<&str> = qname.split(':').collect();
+    let port: u16 = qname[1].parse().map_err(to_ioerror)?;
+    let qname = qname[0];
+    let nameserver: SocketAddr = ([8, 8, 8, 8], 53).into();
+    let mut client = Client::new(ClientConfig::with_nameserver(nameserver)).map_err(to_ioerror)?;
+    let rrset = client
+        .query_rrset::<A>(qname, Class::In)
+        .map_err(to_ioerror)?;
+
+    Ok(rrset
+        .rdata
+        .iter()
+        .map(|a| SocketAddr::new(IpAddr::V4(a.address), port))
+        .collect())
+}
 
 fn main() -> Result<()> {
     Ok(translator()?)
@@ -29,16 +58,17 @@ fn translator() -> Result<()> {
     ]
     .concat();
 
-    let res: serde_json::Value = ureq::get(url.as_str()).call()?.into_json()?;
+    let agent = ureq::builder().resolver(resolve).build();
+    let resp: serde_json::Value = agent.get(url.as_str()).call()?.into_json()?;
 
     print!("translate: ");
-    for s in res["sentences"].as_array().unwrap().iter() {
+    for s in resp["sentences"].as_array().unwrap().iter() {
         print!("{}", s["trans"].as_str().unwrap());
     }
     println!();
 
-    if res["dict"][0]["pos"] != serde_json::Value::Null {
-        for i in res["dict"].as_array().unwrap().iter() {
+    if resp["dict"][0]["pos"] != serde_json::Value::Null {
+        for i in resp["dict"].as_array().unwrap().iter() {
             println!("\n{}:", i["pos"].as_str().unwrap());
             for j in i["terms"].as_array().unwrap().iter() {
                 print!("{}, ", j.as_str().unwrap());
