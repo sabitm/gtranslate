129b6e75ce7d8a5e7ab0dd252d1f0a41ea0fb5b7
sabitm <sabitmaulanaa@gmail.com>
2024-10-23 16:12:24 +0700

    Change hardcoded DNS to optional argument

--- src/helper.rs
+++ src/helper.rs
@@ -1,8 +1,11 @@
 use std::io;
 use std::net::{IpAddr, SocketAddr};
+use std::sync::OnceLock;
 
 use rsdns::clients::{std::Client, ClientConfig};
 use rsdns::{records::data::A, records::Class};
+
+pub static DNS_ADDR: OnceLock<SocketAddr> = OnceLock::new();
 
 fn to_ioerror<T>(err: T) -> io::Error
 where
@@ -15,8 +18,9 @@ pub fn resolve(qname: &str) -> io::Result<Vec<SocketAddr>> {
     let qname: Vec<&str> = qname.split(':').collect();
     let port: u16 = qname[1].parse().map_err(to_ioerror)?;
     let qname = qname[0];
-    let nameserver: SocketAddr = ([8, 8, 8, 8], 53).into();
-    let mut client = Client::new(ClientConfig::with_nameserver(nameserver)).map_err(to_ioerror)?;
+
+    let nameserver = DNS_ADDR.get_or_init(|| ([8, 8, 8, 8], 53).into());
+    let mut client = Client::new(ClientConfig::with_nameserver(*nameserver)).map_err(to_ioerror)?;
     let rrset = client
         .query_rrset::<A>(qname, Class::IN)
         .map_err(to_ioerror)?;
